77f3817036127a75c1fe6511b7d4be0aa7326317
OldMidnight <fareedidris20@gmail.com>
2019-11-15 15:48:59 +0000

    Use Selenium instead of PyQt5

--- API/uploads.py
+++ API/uploads.py
@@ -1,4 +1,7 @@
 from pathlib import Path
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+from time import sleep
 
 from flask import Blueprint, request, jsonify, Response, send_from_directory, current_app
 from flask_jwt_extended import jwt_required, get_jwt_identity, jwt_refresh_token_required, create_access_token
@@ -6,28 +9,35 @@ from API.models import Website, User
 from PySide2.QtWidgets import QApplication
 from PySide2.QtCore import Qt, QUrl, QTimer
 from PySide2.QtWebEngineWidgets import QWebEngineView, QWebEngineSettings
+import threading
 
 bp = Blueprint('uploads', __name__, url_prefix="/uploads")
+options = Options()
+options.headless = True
+driver = webdriver.Firefox(options=options)
 
-class Screenshot(QWebEngineView):
-  def capture(self, url, output_file):
-    self.output_file = output_file
-    self.load(QUrl(url))
-    self.loadFinished.connect(self.on_loaded)
-    # Create hidden view without scrollbars
-    self.setAttribute(Qt.WA_DontShowOnScreen)
-    self.page().settings().setAttribute(
-        QWebEngineSettings.ShowScrollBars, False)
-    self.show()
+# class Screenshot(QWebEngineView):
+#   def capture(self, url, output_file):
+#     print(self.app.thread())
+#     self.output_file = output_file
+#     self.load(QUrl(url))
+#     self.resize(1920, 1080)
+#     self.loadFinished.connect(self.on_loaded)
+#     # Create hidden view without scrollbars
+#     self.setAttribute(Qt.WA_DontShowOnScreen)
+#     self.page().settings().setAttribute(
+#         QWebEngineSettings.ShowScrollBars, False)
+#     self.show()
 
-  def on_loaded(self):
-    self.resize(1920, 1080)
-    # Wait for resize
-    QTimer.singleShot(1000, self.take_screenshot)
+#   def on_loaded(self):
+#     self.resize(1920, 1080)
+#     # Wait for resize
+#     QTimer.singleShot(1000, self.take_screenshot)
 
-  def take_screenshot(self):
-    self.grab().save(self.output_file, 'PNG')
-    self.app.quit()
+
+#   def take_screenshot(self):
+#     self.grab().save(self.output_file, 'PNG')
+#     self.app.quit()
 
 
 @bp.route('/screenshot/grab', methods=('GET',))
@@ -35,14 +45,19 @@ class Screenshot(QWebEngineView):
 def grab_screenshot():
   user_id = get_jwt_identity()
   user = User.query.filter_by(u_id=user_id).first()
-  app = QApplication.instance()
-  if app is None:
-    app = QApplication()
-  s = Screenshot()
-  s.app = app
-  s.capture('http://' + user.domain + '.localhost:3000/', current_app.config['UPLOAD_FOLDER'] + user.domain + '.kreoh.com.png')
-  app.exec_()
-  app.quit()
+  # Qapp = QApplication.instance()
+  # # del Qapp
+  # if Qapp is None:
+  #   Qapp = QApplication([])
+  # s = Screenshot()
+  # s.app = Qapp
+  # s.capture('http://' + user.domain + '.localhost:3000/', current_app.config['UPLOAD_FOLDER'] + user.domain + '.kreoh.com.png')
+  # Qapp.exec_()
+  # Qapp.quit()
+  driver.get('http://' + user.domain + '.localhost:3000/')
+  sleep(1)
+  driver.get_screenshot_as_file(current_app.config['UPLOAD_FOLDER'] + user.domain + '.kreoh.com.png')
+  driver.quit()
   return jsonify(sreenshot_saved=True), 200
 
 @bp.route('/screenshot/<path:filename>', methods=('GET',))
